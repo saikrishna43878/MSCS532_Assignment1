682229ada42bd3067203f15572eda2d94e4473e5
saikrishna43878 <skasthuri43878@ucumberlands.edu>
2026-07-03 23:28:15 +0530

    Insertion Sort Logic Creation

---
+++ Insertion_Sort.py
@@ -0,0 +1,6 @@
+arr = [15, 3, 22, 7, 9, 14]
+for i in range(1, len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j] > arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+print(arr)
